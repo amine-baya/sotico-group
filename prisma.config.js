ff48c721c70ece5c73367784b2de1f7a4b059bce
baya <abaya6054@gmail.com>
2026-04-07 10:14:02 +0100

    URL

--- prisma.config.js
+++ prisma.config.js
@@ -1,6 +1,5 @@
 module.exports = {
   datasource: {
-    // url: 'postgresql://neondb_owner:npg_gufk5PRVFAY6@ep-still-voice-alsxuf36.c-3.eu-central-1.aws.neon.tech/neondb?sslmode=require',
     url: process.env.DATABASE_URL
   },
 }
