4f82720053ae25a945e21f07f8d89fcb81fcc25e
baya <abaya6054@gmail.com>
2026-04-26 17:57:06 +0100

    trustHost and deploy

--- auth.ts
+++ auth.ts
@@ -12,6 +12,7 @@ const credentialsSchema = z.object({
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
   secret: process.env.NEXTAUTH_SECRET,
+  trustHost: true,
   pages: {
     signIn: "/login",
   },
